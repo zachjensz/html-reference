72def9ee8958c5d693d9dacfce6eebfd8f57231d
Zach <zachjensz@protonmail.com>
2021-01-26 23:10:34 +1100

    Added name and description generation

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ const mainSection = document.querySelector('main')
 const cardTemplate = document.querySelector('#element-card')
 elements.forEach((element) => {
     const card = cardTemplate.content.cloneNode(true)
+
     card.querySelector('.tag').innerText = element.tag
+    card.querySelector('.name').innerText = element.name
+    card.querySelector('.description').innerText = element.description
 
     mainSection.append(card)
 })
