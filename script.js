678f9df0f1300aa07fae08983f1d4cf284127306
Zach <zachjensz@protonmail.com>
2021-01-26 23:05:03 +1100

    Set up card template and HTML generation

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 import { elements } from './elements.js'
-console.log(elements)
+
+const mainSection = document.querySelector('main')
+const cardTemplate = document.querySelector('#element-card')
+elements.forEach((element) => {
+    const card = cardTemplate.content.cloneNode(true)
+    card.querySelector('.tag').innerText = element.tag
+
+    mainSection.append(card)
+})
